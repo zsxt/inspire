1a280a99577bfc7a2efbddf718f916b2157fe92b
Zhuo Zihan <sunshine_zzh@163.com>
2015-12-06 23:22:44 -0800

    leaflet example

--- client/views/pages/graphs/leaflet.js
+++ client/views/pages/graphs/leaflet.js
@@ -4,7 +4,11 @@ Template.graphLeaflet.rendered = function(){
 
     var map = L.map('map', {
         doubleClickZoom: false
-    }).setView([49.25044, -123.137], 13);
+    }).setView([39.9388838, 116.3974589], 11);
 
     L.tileLayer.provider('Thunderforest.Outdoors').addTo(map);
+
+    L.marker([39.9388838, 116.3974589]).addTo(map)
+        .bindPopup('中国 北京市 西城区 长安街')
+        .openPopup();
 };
