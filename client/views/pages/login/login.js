dfa2cdd6e76a606a3733e7385968e6b039759b2f
Zhuo Zihan <sunshine_zzh@163.com>
2015-12-09 01:09:11 -0800

    login

---
+++ client/views/pages/login/login.js
@@ -0,0 +1,22 @@
+/**
+ * Created by meteor on 12/8/15.
+ */
+Template.login.events({
+    'submit form': function(e) {
+        var password, username;
+        e.preventDefault();
+        username = $('input[name="username"]').val();
+        password = $('input[name="password"]').val();
+        console.log(username);
+
+        return Meteor.loginWithPassword(username, password, function(error) {
+            if (error) {
+                $("#logintip")[0].setAttribute("class", "alert alert-danger");
+                return $("#logintip")[0].innerHTML = "用户名或者密码不正确";
+            } else {
+                Session.set('username', username);
+                return Router.go('/');
+            }
+        });
+    }
+});
