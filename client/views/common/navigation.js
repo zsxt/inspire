90f215e4e37b14b2af4e4ee58366e89cbb1e6589
Zhuo Zihan <sunshine_zzh@163.com>
2015-12-11 05:53:54 -0800

    judge meteorid

--- client/views/common/navigation.js
+++ client/views/common/navigation.js
@@ -17,28 +17,39 @@ Template.navigation.events({
 Template.navigation.helpers({
 
     userName: function() {
-        return Meteor.user().profile.name;
+        if (Meteor.userId()){
+            return Meteor.user().profile.name;
+        }
     },
 
     userRole: function() {
-        var roles =  Meteor.user().roles;
-        if(roles[0] == 'admin'){
-            return '系统管理员'
-        }
-        else if(roles[0] == 'analyst'){
-            return '数据分析师'
-        }
-        else{
-            return '一般用户'
+        if (Meteor.userId()){
+            var roles =  Meteor.user().roles;
+            if(roles[0] == 'admin'){
+                return '系统管理员'
+            }
+            else if(roles[0] == 'analyst'){
+                return '数据分析师'
+            }
+            else{
+                return '一般用户'
+            }
         }
     },
 
     userAvatar: function() {
-        return Meteor.user().profile.avatar;
+        if (Meteor.userId()){
+            return Meteor.user().profile.avatar;
+        }
     },
 
     isAdmin: function() {
-        return Meteor.user().roles[0] == 'admin'
+        if (Meteor.userId()){
+            return Meteor.user().roles[0] == 'admin'
+        }
+        else{
+            return false;
+        }
     }
 
 });
