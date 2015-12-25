74be39160efbd7560443f0be6d8d507c29ec7708
Zhuo Zihan <sunshine_zzh@163.com>
2015-12-24 16:43:33 -0800

    formatDynamicEventData method

--- server/method.js
+++ server/method.js
@@ -18,5 +18,45 @@ Meteor.methods({
         });
 
         return { 'srcAddr': srcAddr, 'dstAddr': dstAddr};
+    },
+
+    formatDynamicEventData: function(selection, options)  {
+        if (selector && selector.text && selector.text != '') {
+            selector['si'] = new RegExp(selector.text,'i')
+        }
+        delete selector.text;
+
+        if (!options.limit){
+            options.limit = 20; //如果订阅时没有传递limit，默认只有20条
+        }
+
+        var events = Inspire.Collection.IPEvent.find(selection, options).fetch();
+        var results = [];
+
+        events.forEach(function(ipevent) {
+            var srcAddr = Inspire.Collection.IPAddr.findOne({
+                'ipfrom': {$lte: ipevent.ipsrc},
+                'ipto': {$gte: ipevent.ipsrc}
+            });
+
+            var dstAddr = Inspire.Collection.IPAddr.findOne({
+                'ipfrom': {$lte: ipevent.ipdst},
+                'ipto': {$gte: ipevent.ipdst}
+            });
+
+            if(srcAddr && dstAddr){
+                results.push({
+                    'ipsrc': ipevent.ipsrc,
+                    'ipdst': ipevent.ipdst,
+                    'psrc': ipevent.portsrc,
+                    'pdst': ipevent.portdst,
+                    'pro': ipevent.pro,
+                    'srcAddr': srcAddr.addr,
+                    'dstAddr': dstAddr.addr
+                })
+            }
+        });
+
+        return results;
     }
 });
