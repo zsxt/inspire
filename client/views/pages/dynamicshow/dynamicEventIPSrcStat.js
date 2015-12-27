9850a0cda1eac4246ad7b88329058f4838eb44f5
Zhuo Zihan <sunshine_zzh@163.com>
2015-12-27 03:06:09 -0800

    add dynamicEventIPSrcElement template

---
+++ client/views/pages/dynamicshow/dynamicEventIPSrcStat.js
@@ -0,0 +1,11 @@
+Template.dynamicEventIPSrcStat.helpers({
+
+    dynamicEventIPSrc : [
+        {number: 1, labelClass: 'success', content: 'Please contact me', time: '09:00 pm'},
+        {number: 2, labelClass: 'info', content: 'Sign a contract', time: '10:16 am'},
+        {number: 3, labelClass: 'primary', content: 'Open new shop', time: '08.22 pm'},
+        {number: 4, labelClass: 'default', content: 'Call back to Sylvia', time: '11:06 pm'},
+        {number: 5, labelClass: 'primary', content: 'Write a letter to Sandra', time: '12:00 pm'}
+    ]
+
+});
