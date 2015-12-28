9e00324330ff0e97273f03d85f106396721b350b
Zhuo Zihan <sunshine_zzh@163.com>
2015-12-27 16:41:38 -0800

    解决冲突

--- client/collections.js
+++ client/collections.js
@@ -1,8 +1,6 @@
 BaxxStat = new Mongo.Collection('baxx_stat')
 BaxxTimeStat = new Mongo.Collection('baxx_time_stat')
 BaxxGeoStat = new Mongo.Collection('baxx_geo_stat')
-BaxxStat = new Mongo.Collection('baxx_stat');
-BaxxTimeStat = new Mongo.Collection('baxx_time_stat');
 
 Inspire.Collection.IPEventStat = new Mongo.Collection('ipevent_stat');
 
