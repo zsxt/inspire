131e69f85a008fc26a11e451120b6d275fbf5618
Zhuo Zihan <sunshine_zzh@163.com>
2016-01-13 18:37:25 -0800

    remove report

--- client/views/pages/reportShow/reportItem.js
+++ client/views/pages/reportShow/reportItem.js
@@ -6,8 +6,21 @@ Template.reportItem.helpers({
     reportUrl: function(fid){
         var reportFile = Inspire.Collection.ReportFile.findOne(fid);
         if(reportFile){
-            console.log(reportFile.url());
             return reportFile.url();
         }
+    }
+});
+
+
+Template.reportItem.events({
+    'click #removeReport': function(event, template) {
+        var id = this._id;
+        return Inspire.Collection.ReportFile.remove(this.fid, function(error) {
+            if (!error) {
+                return Inspire.Collection.Report.remove(id);
+            } else {
+                return alert(error);
+            }
+        });
     }
 });
