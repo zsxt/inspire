35873192358456de772ec9d2f439708f74d50aee
Ever <ieverx@outlook.com>
2016-03-16 07:27:29 -0700

    default 0 if no data

--- client/views/pages/industry/china.js
+++ client/views/pages/industry/china.js
@@ -8,7 +8,110 @@ Template.industryChina.onCreated(function() {
 Template.industryChina.onRendered(function() {
   var dom = document.getElementById('chart');
   var map = echarts.init(dom);
-  var data = [];
+  var data = [{
+				name : '北京',
+				value : 0
+			}, {
+				name : '天津',
+				value : 0
+			}, {
+				name : '上海',
+				value : 0
+			}, {
+				name : '重庆',
+				value : 0
+			}, {
+				name : '河北',
+				value : 0
+			}, {
+				name : '河南',
+				value : 0
+			}, {
+				name : '云南',
+				value : 0
+			}, {
+				name : '辽宁',
+				value : 0
+			}, {
+				name : '黑龙江',
+				value : 0
+			}, {
+				name : '湖南',
+				value : 0
+			}, {
+				name : '安徽',
+				value : 0
+			}, {
+				name : '山东',
+				value : 0
+			}, {
+				name : '新疆',
+				value : 0
+			}, {
+				name : '江苏',
+				value : 0
+			}, {
+				name : '浙江',
+				value : 0
+			}, {
+				name : '江西',
+				value : 0
+			}, {
+				name : '湖北',
+				value : 0
+			}, {
+				name : '广西',
+				value : 0
+			}, {
+				name : '甘肃',
+				value : 0
+			}, {
+				name : '山西',
+				value : 0
+			}, {
+				name : '内蒙古',
+				value : 0
+			}, {
+				name : '陕西',
+				value : 0
+			}, {
+				name : '吉林',
+				value :0
+			}, {
+				name : '福建',
+				value : 0
+			}, {
+				name : '贵州',
+				value : 0
+			}, {
+				name : '广东',
+				value : 0
+			}, {
+				name : '青海',
+				value : 0
+			}, {
+				name : '西藏',
+				value : 0
+			}, {
+				name : '四川',
+				value : 0
+			}, {
+				name : '宁夏',
+				value : 0
+			}, {
+				name : '海南',
+				value : 0
+			}, {
+				name : '台湾',
+				value : 0
+			}, {
+				name : '香港',
+				value : 0
+			}, {
+				name : '澳门',
+				value : 0
+			}
+		];
 
   var mapOption = {
 		//backgroundColor:'rgba(232, 232, 232, 1)',
@@ -125,9 +228,15 @@ Template.industryChina.onRendered(function() {
 
   this.autorun(function() {
     var result = IndustryControlChina.find({});
-    console.log(result.fetch());
-    data = result.fetch();
+    var newData = result.fetch();
+
+    var cities = _.pluck(newData, 'name');
+    data = _.reject(data, function (d) {
+      return _.contains(cities, d.name);
+    })
+    data = _.union(data, newData);
     mapOption.series[0].data = data;
+
     var m = 0;
     for (var i = 0; i < data.length; ++i) {
       if (data[i].value > m) {
