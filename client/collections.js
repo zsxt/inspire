3c13b9d224ba802beaa45c3b8e7d0d73e643be95
Ever <ieverx@outlook.com>
2016-03-16 06:48:14 -0700

    fix conflict

--- client/collections.js
+++ client/collections.js
@@ -5,10 +5,7 @@ BaxxTotal = new Mongo.Collection('baxx_total')
 
 Inspire.Collection.IPEventStat = new Mongo.Collection('ipevent_stat');
 Inspire.Collection.IPAddrStat = new Mongo.Collection('ipaddr_stat');
-<<<<<<< HEAD
 Inspire.Collection.WebScanStatWorldStat = new Mongo.Collection('webscan_statworld_stat');
 Inspire.Collection.WebScanStatChinaStat = new Mongo.Collection('webscan_statchina_stat');
-=======
->>>>>>> ae23bdd4b6eb65ddffd2c9e80833e6a1d0e8d9c0
 
 IndustryControlChina = new Mongo.Collection('industry_control_china')
