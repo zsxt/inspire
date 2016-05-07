6d10c974aa79b6a25b2b3c521dcd37b46af1f701
Ever <ieverx@outlook.com>
2016-05-07 00:35:09 -0700

    add echarts3, last add failed

---
+++ packages/meteor-echarts/package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: 'ieverx:echarts3',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Use echarts with meteor.',
+  // URL to the Git repository containing the source code for this package.
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.addFiles(['echarts.min.js', 'china.js', 'world.js'], 'client', {bare:true});
+});
