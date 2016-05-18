93ce6a754ed06fd678e2b5933814b16895b782b0
monkback <monkback@gmail.com>
2016-05-18 10:19:06 +0800

    attack: time

--- client/views/pages/industry/attack.js
+++ client/views/pages/industry/attack.js
@@ -167,7 +167,22 @@ Template.industryAttack.onRendered(function() {
       }
     });
   }
-  
+  Date.prototype.format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
   function start(ip) {
     Session['image'] = [];
     clear();
@@ -176,8 +191,17 @@ Template.industryAttack.onRendered(function() {
         fetchImages(ip);
       }
       if (index < Session['image'].length) {
-        $('#load_img').attr('src', '/industry/attackimg/' + ip + '/' + Session['image'][index]);
-        index++;
+        var d = new Date() - 240000;
+        var fname = new Date(d).format('yyyy-MM-dd HH_mm_ss');
+        fname = current + '.jpg';
+        if (fname >= Session['image'][index]) {
+          if (index + 1 == Session['image'].length || fname < Session['image'][index + 1]) {
+            $('#load_img').attr('src', '/industry/attackimg/' + ip + '/' + Session['image'][index]);
+            index++;
+          } else {
+            index++;
+          }
+        }
       }
     }, 1000);
   }
