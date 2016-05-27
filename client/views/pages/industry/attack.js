77da8fc5ea56ee3d7d23a1225474b77b7d2b72af
monkback <monkback@gmail.com>
2016-05-27 15:50:27 +0800

    img

--- client/views/pages/industry/attack.js
+++ client/views/pages/industry/attack.js
@@ -2,6 +2,7 @@ Template.industryAttack.onRendered(function () {
   var currentIp = location.hash;
   if (currentIp) {
     $('input[name="ip"]').val(currentIp.substring(1));
+    $('#load_img').attr('src','/industry/attack/' + '#' + 'currentIp.substring(1)');
   }
 
   /*var Kort = (function () {
